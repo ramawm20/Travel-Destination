519bbddf42bccd8398578d2348366a5e89e62d96
ramawm20 <ramawael54@gmail.com>
2023-04-19 17:43:38 +0000

    class 11 solved

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+function Header(){
+    return(
+        <>
+        <div id="header">
+        <h1>Travel Destination</h1>
+        <br/>
+        </div>
+        </>
+    )
+}
+export default Header;
---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import './Home.css'
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+
+function Home() {
+    
+    return (
+        <>
+            <div>
+                <Header/>
+                <Footer />
+            </div>
+        </>
+    )
+}
+export default Home;
